dfe2746ff06cf2ae694823dbaff04c9eed5a4e43
thanuja004 <thanujak004@gmail.com>
2026-09-24 14:53:37 +0530

    added new print statement

--- src/com/Employee.java
+++ src/com/Employee.java
@@ -1,9 +1,11 @@
 package com;
 
 public class Employee {
-
+    void show() {
+    	
+    }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/com/Sample.java
+++ src/com/Sample.java
@@ -3,8 +3,9 @@ package com;
 public class Sample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		System.out.println("helo java");
+		System.out.println("my new file");
+		System.out.println("my new page");
+		}
 
 }
